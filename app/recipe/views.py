9789d0635ad92f185e86fca443aeed0ba88e1969
Max Khrichtchatyi <maxim.khrichtchatyi@gmail.com>
2021-09-26 12:12:35 +0600

    feat: 🏈 add custom recipe detail view

--- app/recipe/views.py
+++ app/recipe/views.py
@@ -46,3 +46,10 @@ class RecipeViewSet(viewsets.ModelViewSet):
     def get_queryset(self):
         """Retrieve the recipes for the authenticated used"""
         return self.queryset.filter(user=self.request.user).order_by('-id')
+
+    def get_serializer_class(self):
+        """Return appropriate serializer class"""
+        if self.action == 'retrieve':
+            return serializers.RecipeDetailSerializer
+
+        return self.serializer_class
